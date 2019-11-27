cb01d4f4f3d36da6c047ae2bfa5370a85fe554b1
stsogoo <stsogoo@vmware.com>
2019-11-26 18:04:08 -0800

    fix: dnd errors
    Signed-off-by: stsogoo <stsogoo@vmware.com>

--- src/clr-angular/utils/drag-and-drop/all.spec.ts
+++ src/clr-angular/utils/drag-and-drop/all.spec.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016-2018 VMware, Inc. All Rights Reserved.
+ * Copyright (c) 2016-2019 VMware, Inc. All Rights Reserved.
  * This software is released under MIT license.
  * The full license information can be found in LICENSE in the root directory of this project.
  */
@@ -18,7 +18,7 @@ import ClrDragHandleRegistrarSpecs from './providers/drag-handle-registrar.servi
 import ClrDraggableSnapshotSpecs from './providers/draggable-snapshot.service.spec';
 import ClrGlobalDragModeSpecs from './providers/global-drag-mode.service.spec';
 
-describe('Drag And Drop', function() {
+fdescribe('Drag And Drop', function() {
   describe('Providers', function() {
     ClrDragAndDropEventBusSpecs();
     ClrDragEventListenerSpecs();
--- src/clr-angular/utils/drag-and-drop/draggable/draggable.spec.ts
+++ src/clr-angular/utils/drag-and-drop/draggable/draggable.spec.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016-2018 VMware, Inc. All Rights Reserved.
+ * Copyright (c) 2016-2019 VMware, Inc. All Rights Reserved.
  * This software is released under MIT license.
  * The full license information can be found in LICENSE in the root directory of this project.
  */
@@ -43,7 +43,7 @@ export default function(): void {
         imports: [ClrDragAndDropModule, NoopAnimationsModule],
         declarations: [BasicDraggableTest],
       });
-      TestBed.overrideComponent(ClrDraggable, {
+      TestBed.overrideDirective(ClrDraggable, {
         set: {
           providers: [
             DomAdapter,
